f2eb9dc8eb171a73766e922461e7707157e405c6
oleHxOleksuk <108345191+oleHxOleksuk@users.noreply.github.com>
2023-02-25 16:50:10 +0200

    fet4

--- src/redux/contacts/contacts-reducer.js
+++ src/redux/contacts/contacts-reducer.js
@@ -3,7 +3,9 @@ import { createReducer } from "@reduxjs/toolkit";
 import { addContact, deleteContact } from "./contacts-action";
 
 const contactsReducer = createReducer([], {
-  [addContact]: (state, {payload}) => [...state, payload ],
+  [addContact]: (state, {payload}) => {
+    state.push(payload)
+  },
   [deleteContact]: (state, {payload}) => state.filter(item => item.id !== payload)
 })
 
